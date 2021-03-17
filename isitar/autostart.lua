3a684f53ea6cd6956fb4d4e9998c6265eba5185b
Isitar <luescherpascal@gmail.com>
2021-03-17 07:39:54 +0100

    numlock on on startup

--- isitar/autostart.lua
+++ isitar/autostart.lua
@@ -6,8 +6,13 @@ function A.autostart()
     -- transition / animation
     awful.spawn.with_shell("compton")
 
+    -- numlock on
+    awful.spawn.with_shell("numlockx on")
+
     -- spotify
     awful.spawn("spotify")
+
+
     -- awful.spawn("cvlc ~/Projects/KRK_stayawake/10hz_tone.wav --repeat")
     -- awful.spawn("gnome-terminal --command=\"cvlc Projects/KRK_stayawake/10hz_tone.wav --repeat\"", {
     --    tag = "music",
